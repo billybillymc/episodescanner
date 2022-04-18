03445bb14e78e60da4eb16903e5744f9c49aa08f
billybillymc <mcintyre.w@gmail.com>
2022-04-18 14:47:22 -0700

    Added input fields for CLI.

--- pyscanner.py
+++ pyscanner.py
@@ -12,12 +12,15 @@ chrome_options = Options()
 chromedriver_autoinstaller.install()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', "Oct", "Nov", "Dec"]
-for i in range(1,34):
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+yt_code = input('YouTube code?      ') ## Add the YT code, like SC7D01nDpcPH0v9jgEFxCykg
+show_name = input('Urlname?     ') ## Add the show name, like its-always-sunny-in-philadelphia
+last = int(input('Number of seasons?       ')) ## number of seasons, like 15
+for i in range(1,last+1):
     dates=[]
     titles=[]
     runtimes=[]
-    driver.get(f"https://www.youtube.com/show/SCSKE3leoX9KNm23E0nkTSFA?season={i}")
+    driver.get(f"https://www.youtube.com/show/{yt_code}?season={i}")
     driver.find_element_by_tag_name('body').send_keys(Keys.END)
     driver.find_element_by_tag_name('body').send_keys(Keys.END)
     time.sleep(3)
@@ -49,11 +52,11 @@ for i in range(1,34):
                 new_date=re.findall(f"{t} \d+, \d+",count.text)
                 dates.append(new_date[0])
                 break
-    path = 'C:/Users/mcint/py/Outputs/' + str(i)
+    path = 'C:/Users/mcint/py/episodes_' + show_name + '/' + str(i)
     isdir = os.path.isdir(path)
     if isdir == False:
         os.makedirs(path)
     for final in range(0,length):
         data={"number of episode":final+1,"date":dates[final],"runtime":runtimes[final],"episode_name":titles[final]}
-        with open(f"outputs/{i}/{final+1}.json",'w+') as f:
+        with open(f"{path}/{final+1}.json",'w+') as f:
             json.dump(data,f)
