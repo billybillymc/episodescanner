2fee07a787a44a6d40f85b45a2ddee7d44214618
alimed123 <92314175+alimed123@users.noreply.github.com>
2022-04-18 20:45:50 +0100

    Update pyscanner.py

--- pyscanner.py
+++ pyscanner.py
@@ -18,6 +18,7 @@ for i in range(1,34):
     tt=tt+1
     driver.get(f"https://www.youtube.com/show/SCSKE3leoX9KNm23E0nkTSFA?season={i}")
     driver.find_element_by_tag_name('body').send_keys(Keys.END)
+    driver.find_element_by_tag_name('body').send_keys(Keys.END)
     time.sleep(3)
     length=len(driver.find_elements_by_xpath("//a[@id='video-title']"))
     #getting_title
@@ -25,6 +26,13 @@ for i in range(1,34):
         titles.append(count.text)
     for count in driver.find_elements_by_xpath("//span[@id='text']"):
         runtimes.append(count.text)
+    while '' in runtimes:
+        runtimes=[]
+        driver.refresh()
+        driver.find_element_by_tag_name('body').send_keys(Keys.END)
+        time.sleep(3)
+        for count in driver.find_elements_by_xpath("//span[@id='text']"):
+            runtimes.append(count.text)
     try:
         for count in driver.find_elements_by_xpath("//*[@class='style-scope ytd-channel-name']"):
             for t in months:
@@ -40,9 +48,7 @@ for i in range(1,34):
                 new_date=re.findall(f"{t} \d+, \d+",count.text)
                 dates.append(new_date[0])
                 break
-    p=[]
-    p=[*range(0,length)]
     for final in range(0,length):
-        p[final]=(f"{{ {final+1},'{dates[final]}','{runtimes[final]}','{titles[final]}'}}")
+        data={"number of episode":final+1,"date":dates[final],"runtime":runtimes[final],"episode_name":titles[final]}
         with open(f"outputs/{i}/{final+1}.json",'w+') as f:
-            json.dump(p[final],f)
+            json.dump(data,f)
